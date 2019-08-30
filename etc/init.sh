058ca8adf3c535ed7d698761110f9acaf09aa0c9
Alex <wowgss@gmail.com>
2019-08-30 19:28:02 +0500

    bug

--- etc/init.sh
+++ etc/init.sh
@@ -1,3 +1,3 @@
 sudo ln -s /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/test.conf
-sudo rm -f /etc/nginx/sites-enable/default
+sudo rm -f /etc/nginx/sites-enabled/default
 sudo /etc/init.d/nginx restart
