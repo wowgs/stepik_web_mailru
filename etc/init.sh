5a834c40f35d78d15f31a847a33988b9e41e4cd4
Alex <wowgss@gmail.com>
2019-09-07 09:34:37 +0500

    task2

--- etc/init.sh
+++ etc/init.sh
@@ -3,7 +3,5 @@ sudo rm -f /etc/nginx/sites-enabled/default
 sudo /etc/init.d/nginx restart
 sudo apt update
 sudo apt install -y gunicorn
-sudo apt install -y pip3
-sudo pip3 install -y -r /home/box/web/etc/requirements.txt
+sudo pip3 install -r /home/box/web/etc/requirements.txt
 sudo ln -s /home/box/web/etc/hello.py /etc/gunicorn.d/hello.py
-sudo gunicorn -c /etc/gunicorn.d/hello.py /home/box/web/wsgi:app
